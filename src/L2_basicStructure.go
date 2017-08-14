0df3585832de21bcff1a93338a299bec195336f2
Wich <zhwzhsky@163.com>
2017-08-14 22:09:19 +0800

    Lesson2: study basic structure

---
+++ src/L2_basicStructure.go
@@ -0,0 +1,25 @@
+// 当前程序的包名
+package main
+
+// 导入其他的包
+import "fmt"
+
+// 常量的定义
+const pi = 3.14
+
+//全局变量的定义与赋值
+var name = "gopher"
+
+//一般类型声明
+type myType int
+
+//结构声明
+type gopher struct{}
+
+// 接口声明
+type golang interface{}
+
+// 函数
+func main() {
+	fmt.Println("Hello World! 你好，世界！")
+}
