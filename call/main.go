f0de721b4244da1e3b71e25417eeb943930b9924
Wich <zhwzhsky@163.com>
2017-08-20 17:36:39 +0800

    文件目录结构更改

---
+++ call/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/zhwzhsky/GoWorkplace/goTest"
+)
+
+func main() {
+	goTest.Hello()
+}
---
+++ goTest/goTest.go
@@ -0,0 +1,14 @@
+package goTest
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	hi()
+	fmt.Println("Hello from goTest.")
+}
+
+func hi() {
+	fmt.Println("Hi from goTest.")
+}
