5f48121a131575525a6d2945d8b3325f58d68c4a
Wich <zhwzhsky@163.com>
2017-08-20 16:29:45 +0800

    Lesson14:并发

--- src/L14_.go
+++ src/L14_.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	// "time"
 )
 
 func main() {
-
+	c := make(chan bool)
+	go func() {
+		fmt.Println("Go Go Go!!!")
+		c <- true //存入channel中
+		close(c)
+	}()
+	for v := range c {
+		fmt.Println(v)
+	}
 }
